cbe3f78cd1998df789c0008bf7a11f9cf370b85a
wwpedro <pedrinhosilva35997@gmail.com>
2023-11-26 19:11:48 -0300

    adicionado estilo na pagina de login e detalhes

--- lib/screens/details_book.dart
+++ lib/screens/details_book.dart
@@ -1,49 +1,81 @@
 import 'package:desafio2pedro/models/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BookDetailPage extends StatelessWidget {
   final BookModel book;
   const BookDetailPage({Key? key, required this.book});
+
+  Future<void> _launchUrl(String _url) async {
+    if (!await launch(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Detalhe Livros"),
+        centerTitle: true,
       ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 16.0,
-            ),
-            Center(
-              child: Image.network(
-                book.cover_url,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, Colors.blue],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 16.0,
               ),
-            ),
-            const SizedBox(
-              height: 22.0,
-            ),
-            Text(
-              book.title,
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Baixar"),
+              Center(
+                child: Image.network(
+                  book.cover_url,
                 ),
-              ],
-            )
-          ],
+              ),
+              const SizedBox(
+                height: 22.0,
+              ),
+              Text(
+                book.title,
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  String bookUrl = book.download_url;
+                  _launchUrl(bookUrl);
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.blue,
+                  textStyle: TextStyle(fontSize: 18.0),
+                ),
+                child: Text("Baixar"),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.blue,
+                  textStyle: TextStyle(fontSize: 18.0),
+                ),
+                child: Text("Outro Botão"),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,77 @@
+import 'package:desafio2pedro/controllers/book_controller.dart';
+import 'package:desafio2pedro/models/book_model.dart';
+import 'package:desafio2pedro/screens/components/list_books.dart';
+import 'package:desafio2pedro/screens/components/load.dart';
+import 'package:desafio2pedro/screens/components/search.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<BookModel> _books = <BookModel>[];
+  List<BookModel> _booksDisplay = <BookModel>[];
+
+  bool _isloading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBooks().then((value) {
+      setState(() {
+        _isloading = false;
+        _books.addAll(value);
+        _booksDisplay = _books;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("home tela"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Livraria Online"),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, Colors.blue],
+          ),
+        ),
+        child: SafeArea(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              if (!_isloading) {
+                return index == 0
+                    ? SearchComponent(
+                        hintText: "nome",
+                        onChanged: (searchtext) {
+                          searchtext = searchtext.toLowerCase();
+                          setState(() {
+                            _booksDisplay = _books.where((element) {
+                              var nameLowerCase = element.title.toLowerCase();
+                              return nameLowerCase.contains(searchtext);
+                            }).toList();
+                          });
+                        },
+                      )
+                    : ListBooks(book: _booksDisplay[index]);
+              } else {
+                return const LoadPage();
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
